a49b1b770330b04592d1ef17a160b319f4f00e12
Michał Kruszewski <mkru@protonmail.com>
2024-07-26 07:24:13 +0200

    Some keybindings ideas

--- internal/cfg/keybindings.go
+++ internal/cfg/keybindings.go
@@ -11,6 +11,15 @@ type Keybindings map[string]string
 // KeybindingsDefault returns default keybindings.
 func KeybindingsDefault() Keybindings {
 	return map[string]string{
+		// Alternation
+		"Alt+Up":    "move-up",
+		"Alt+Down":  "move-down",
+		"Alt+Left":  "indent-decrease",
+		"Alt+Right": "indent-increase",
+		"Alt+R":     "replace",
+		"Alt+U":     "undo",
+		"Alt+J":     "line-join-below",
+		"Alt+K":     "line-join-above",
 		// Cmd
 		"Ctrl+E": "cmd",
 		// Cursor
@@ -21,24 +30,32 @@ func KeybindingsDefault() Keybindings {
 		"Ctrl+Left":  "cursor-word-start",
 		"Ctrl+Right": "cursor-word-end",
 		"Ctrl+A":     "cursor-line-start",
+		// Deletion
+		"Backspace":  "backspace",
+		"Backspace2": "backspace",
+		"Delete":     "del",
+		"Alt+W":      "del-word",
+		"Alt+{":      "del-within-brace",
+		"Alt+[":      "del-within-bracket",
+		"Alt+(":      "del-within-paren",
 		// File
 		"Ctrl+O": "file-open",
 		"Ctrl+S": "file-save",
 		// Tab
 		"Ctrl+T": "tab-open",
 		// Miscellaneous
-		"Backspace":  "backspace",
-		"Backspace2": "backspace",
-		"Delete":     "del",
 		"Enter":      "enter",
 		"Esc":        "escape",
 		"Ctrl+C":     "copy",
 		"Ctrl+F":     "find",
 		"Ctrl+H":     "help",
 		"Ctrl+Q":     "quit",
-		"Ctrl+U":     "undo",
-		"Ctrl+V":     "paste",
-		"Ctrl+W":     "sel-word",
+		// "Ctrl+U":     "undo", Alt+U
+		"Ctrl+V": "paste",
+		"Ctrl+W": "sel-word",
+		// Selection
+		"Ctrl+K": "sel-line-end",
+		"Ctrl+L": "sel-line",
 	}
 }
 
