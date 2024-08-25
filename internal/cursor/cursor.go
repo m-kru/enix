66d8ecb5b184745e15e5b03e2e5f36f5ad26a8c5
Michał Kruszewski <mkru@protonmail.com>
2024-08-25 08:30:35 +0200

    Minor formatting changes

--- internal/cursor/cursor.go
+++ internal/cursor/cursor.go
@@ -2,7 +2,6 @@ package cursor
 
 import (
 	"github.com/m-kru/enix/internal/cfg"
-	"github.com/m-kru/enix/internal/frame"
 	"github.com/m-kru/enix/internal/line"
 	"github.com/m-kru/enix/internal/view"
 
@@ -210,25 +209,4 @@ func (c *Cursor) Delete() {
 	}
 
 	c.Line.Delete(c.BufIdx, 1)
-}
-
-func (c *Cursor) Render(
-	config *cfg.Config,
-	colors *cfg.Colorscheme,
-	frame frame.Frame,
-	view view.View,
-	primary bool,
-) {
-	x := c.Line.ColumnIdx(c.BufIdx, config.TabWidth) - view.Column
-	/*
-		if x >= frame.Width {
-			return
-		}
-	*/
-	if primary {
-		frame.ShowCursor(x, 0)
-	} else {
-		r := frame.GetContent(x, 0)
-		frame.SetContent(x, 0, r, colors.Cursor)
-	}
 }
---
+++ internal/cursor/render.go
@@ -0,0 +1,28 @@
+package cursor
+
+import (
+	"github.com/m-kru/enix/internal/cfg"
+	"github.com/m-kru/enix/internal/frame"
+	"github.com/m-kru/enix/internal/view"
+)
+
+func (c *Cursor) Render(
+	config *cfg.Config,
+	colors *cfg.Colorscheme,
+	frame frame.Frame,
+	view view.View,
+	primary bool,
+) {
+	x := c.Line.ColumnIdx(c.BufIdx, config.TabWidth) - view.Column
+	/*
+		if x >= frame.Width {
+			return
+		}
+	*/
+	if primary {
+		frame.ShowCursor(x, 0)
+	} else {
+		r := frame.GetContent(x, 0)
+		frame.SetContent(x, 0, r, colors.Cursor)
+	}
+}
