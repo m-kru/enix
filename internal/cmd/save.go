40602ebc944d5ab69b80b8d3584bff2e32904280
Michał Kruszewski <mkru@protonmail.com>
2024-09-23 15:16:19 +0200

    Add ability to save tab to different file

--- internal/cmd/save.go
+++ internal/cmd/save.go
@@ -8,8 +8,13 @@ import (
 )
 
 func Save(args []string, tab *tab.Tab, trim bool) error {
-	if len(args) > 0 {
-		return fmt.Errorf("save: expected 0 args, provided %d", len(args))
+	if len(args) > 1 {
+		return fmt.Errorf("save: expected at most 1 arg, provided %d", len(args))
+	}
+
+	path := tab.Path
+	if len(args) == 1 {
+		path = args[0]
 	}
 
 	if trim {
@@ -17,14 +22,14 @@ func Save(args []string, tab *tab.Tab, trim bool) error {
 	}
 
 	if tab.Config.SafeFileSave {
-		return safeSave(tab)
+		return safeSave(tab, path)
 	} else {
-		return save(tab)
+		return save(tab, path)
 	}
 }
 
-func save(tab *tab.Tab) error {
-	file, err := os.OpenFile(tab.Path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+func save(tab *tab.Tab, path string) error {
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
 		return fmt.Errorf("save: %v", err)
 	}
@@ -42,6 +47,6 @@ func save(tab *tab.Tab) error {
 	return nil
 }
 
-func safeSave(tab *tab.Tab) error {
+func safeSave(tab *tab.Tab, path string) error {
 	return nil
 }
