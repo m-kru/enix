23b2905921fb80ca36295fb13f8ec29edffd754f
Michał Kruszewski <mkru@protonmail.com>
2024-10-09 16:28:44 +0200

    cfg: Add function for reading colors from JSON

--- internal/cfg/colors.go
+++ internal/cfg/colors.go
@@ -1,5 +1,16 @@
 package cfg
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/m-kru/enix/internal/arg"
+)
+
 type Colors struct {
 	Background    uint64
 	Foreground    uint64
@@ -23,7 +34,71 @@ type Colors struct {
 
 // Read reads colors from file named "name.json".
 func Read(name string) (Colors, error) {
-	c := Colors{}
+
+	colorsDir := filepath.Join(ConfigDir, "colors")
+	if arg.ColorsDir != "" {
+		colorsDir = arg.ColorsDir
+	}
+
+	path := filepath.Join(colorsDir, name+".json")
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("opening colors file: %v", err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("reading colors file: %v", err)
+	}
+
+	var colors map[string]any
+	err = json.Unmarshal([]byte(data), &colors)
+	if err != nil {
+		log.Fatalf("unmarshalling colors file: %v", err)
+	}
+
+	c := Colors{
+		getColorFromJSON(colors, "Background"),
+		getColorFromJSON(colors, "Foreground"),
+		getColorFromJSON(colors, "Black"),
+		getColorFromJSON(colors, "Red"),
+		getColorFromJSON(colors, "Green"),
+		getColorFromJSON(colors, "Yellow"),
+		getColorFromJSON(colors, "Blue"),
+		getColorFromJSON(colors, "Magenta"),
+		getColorFromJSON(colors, "Cyan"),
+		getColorFromJSON(colors, "White"),
+		getColorFromJSON(colors, "BrightBlack"),
+		getColorFromJSON(colors, "BrightRed"),
+		getColorFromJSON(colors, "BrightGreen"),
+		getColorFromJSON(colors, "BrightYellow"),
+		getColorFromJSON(colors, "BrightBlue"),
+		getColorFromJSON(colors, "BrightMagenta"),
+		getColorFromJSON(colors, "BrightCyan"),
+		getColorFromJSON(colors, "BrightWhite"),
+	}
 
 	return c, nil
+}
+
+func getColorFromJSON(colors map[string]any, name string) uint64 {
+	var val uint64
+	var err error
+
+	if value, ok := colors["Background"]; ok {
+		if hex, ok := value.(string); ok {
+			val, err = strconv.ParseUint(hex, 16, 64)
+			if err != nil {
+				log.Fatalf("can't convert value for %s color: %v", name, err)
+			}
+		} else {
+			log.Fatalf("invalid value type for %s color, expected string", name)
+		}
+	} else {
+		log.Fatalf("missing definition of %s color", name)
+	}
+
+	return val
 }
