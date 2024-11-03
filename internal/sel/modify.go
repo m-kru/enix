adaea239b552af154a4c8b843a73024600af7faf
Michał Kruszewski <mkru@protonmail.com>
2024-11-03 19:59:22 +0100

    sel: Implement right to next line

--- internal/sel/modify.go
+++ internal/sel/modify.go
@@ -20,5 +20,20 @@ func (s *Selection) rightCursorOnRight() {
 		return
 	}
 
-	panic("unimplemented")
+	if s.Line.Next == nil {
+		// Do nothing, this is end of text
+		return
+	}
+
+	newS := &Selection{
+		Line:         s.Line.Next,
+		LineNum:      s.LineNum + 1,
+		StartRuneIdx: 0,
+		EndRuneIdx:   0,
+		CursorIdx:    0,
+	}
+
+	newS.Prev = s
+	s.Next = newS
+	s.CursorIdx = -1
 }
