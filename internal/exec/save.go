8a11d6b0012753cd561e5504e9ae0c6830a2969f
Michał Kruszewski <mkru@protonmail.com>
2024-11-08 12:07:48 +0100

    exec: Implement safe file save

--- internal/exec/save.go
+++ internal/exec/save.go
@@ -1,6 +1,7 @@
 package exec
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -48,5 +49,39 @@ func save(tab *tab.Tab, path string) (string, error) {
 }
 
 func safeSave(tab *tab.Tab, path string) (string, error) {
+	// If file doesn't exist use regular save.
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		return save(tab, path)
+	}
+
+	// First write content to the backup file
+	backupPath := path + ".enix-bak"
+	backupFile, err := os.OpenFile(backupPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	if err != nil {
+		return "", fmt.Errorf("save: %v", err)
+	}
+
+	err = tab.Save(backupFile)
+	if err != nil {
+		return "", fmt.Errorf("save: %v", err)
+	}
+
+	err = backupFile.Close()
+	if err != nil {
+		return "", fmt.Errorf("save: %v", err)
+	}
+
+	// Write content to the root file
+	_, err = save(tab, path)
+	if err != nil {
+		return "", err
+	}
+
+	// Remove backup file
+	err = os.Remove(backupPath)
+	if err != nil {
+		return "", err
+	}
+
 	return fmt.Sprintf("tab saved to file %s", path), nil
 }
