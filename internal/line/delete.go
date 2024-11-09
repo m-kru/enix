89c30cb58cbfb1a9709fa163aef0c75723929249
Michał Kruszewski <mkru@protonmail.com>
2024-11-09 15:52:38 +0100

    cursor: Fix some delete bug

--- internal/line/delete.go
+++ internal/line/delete.go
@@ -1,7 +1,9 @@
 package line
 
-func (l *Line) DeleteRune(idx int) *Line {
-	if idx == l.RuneCount() {
+import "unicode/utf8"
+
+func (l *Line) DeleteRune(runeIdx int) *Line {
+	if runeIdx == l.RuneCount() {
 		if l.Next == nil {
 			return nil
 		}
@@ -10,7 +12,10 @@ func (l *Line) DeleteRune(idx int) *Line {
 		return l.Join(false)
 	}
 
-	l.Buf = append(l.Buf[:idx], l.Buf[idx+1:]...)
+	bIdx := l.BufIdx(runeIdx)
+	_, rLen := utf8.DecodeRune(l.Buf[bIdx:])
+
+	l.Buf = append(l.Buf[:bIdx], l.Buf[bIdx+rLen:]...)
 
 	return nil
 }
