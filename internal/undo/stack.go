c00c244a8bcb8bfe4e1ce3aeff1489b93bf7fa33
Michał Kruszewski <mkru@protonmail.com>
2024-11-14 14:55:03 +0100

    Add undo package

---
+++ internal/undo/stack.go
@@ -0,0 +1,21 @@
+package undo
+
+type Stack struct {
+	len int
+	cap int
+	first *
+	last *
+}
+
+func NewStack(cap int) *Stack {
+	return &Stack{cap: cap}
+}
+
+func (s *Stack) Push() {
+
+
+}
+
+func (s *Stack) Pop() {
+
+}
