b35c858f045806035325f4d2ae604f5d0c9d5286
Michał Kruszewski <mkru@protonmail.com>
2025-01-06 20:12:44 +0100

    help: Add cursors topic

--- internal/help/topic.go
+++ internal/help/topic.go
@@ -10,9 +10,26 @@ Namely, the 'insert' commamnd (and its derivatives), there is no 'append'.
 There is no custom plugin system.
 However, it is possible to pass selections as standard input to external programs.
 The selections are then replaced with the program standard output.
-This is achieved using the 'sh' command (not yet implemented).
+This is achieved using the 'sh' command.
 Enter 'help sh' command for more details.
 `,
+
+	"cursors": `A tab at any time has at least one cursor or selection.
+The number of cursors is unlimited.
+Each cursor points to a line and rune index or line end.
+The line end rune is displayed in the tab.
+However, it is not part of the tab content.
+It solely symbolizes the line end, not the newline.
+
+At no point more than one cursor can point to the same position.
+If, due to command execution, multiple cursors point to the same position, some of them are desroyed so that only one cursor pointing to a particular position exists.
+
+The status bar displays position of the cursor on the right side in the following format: [line number]:[column].
+In the case of multiple cursors, position of the last cursor is displayed.
+The last cursor is the cursor which was created as the last one.
+It is not necessarily cursor with the greatest line number or column position.
+
+In the case of escaping from multiple cursors by executing the 'esc' command, it is only the last cursor that survives.`,
 
 	"keybindings": `There are three independent keybinding sets.
 Each of them is used in a different context:
@@ -20,18 +37,17 @@ Each of them is used in a different context:
 2. insert keys - keybindings for a tab in the insert mode,
 3. prompt keys - keybindings for the prompt.
 
-Enix tries to offer sane default keybindings so that the user doesn't have to
-configure much out of the box and can use the editor straight after installation
-on various machines.
+Enix tries to offer sane default keybindings so the user doesn't have to configure much out of the box.
+The user can start using the editor right after installing it on various machines.
 
 To get currently set keybindings, call enix with one of the following flags:
 '-dump-keys', '-dump-insert-keys', or '-dump-prompt-keys'.
 
 The default keybindings try to follow the following rules (with some exceptions):
-- The most common commands are under primary keys so that the actions associated
-  with them can be executed with a single keystroke.
-- Keybindings with the Ctrl modifier are used to control things. For example,
-  spawn new cursors, scroll view, create or iterate tabs.
+- The most common commands are under primary keys
+  The actions associated with them can be executed with a single keystroke.
+- Keybindings with the Ctrl modifier are used to control things.
+  For example, spawn new cursors, scroll view, create or iterate tabs.
 - Keybindings with the Alt modifier are used to alternate the text.
   For example, join or move lines up or down.
 - Keybindings with the Shift modifier are used to extend or shrink selections.
@@ -57,9 +73,8 @@ To get more details, open enix and execute the 'help key-name' command.
 Risky keybindings
 
 Enix receives key events from a terminal emulator, not from the keyboard.
-As terminal emulators are weird and non-uniform applications, they eat or
-replace some key events. This is why some keybindings with the Ctrl modifier
-never reach enix.
+As terminal emulators are weird and non-uniform applications, they eat or replace some key events.
+This is why some keybindings with the Ctrl modifier never reach enix.
 
 The following Ctrl keybindings are considered risky and are advised to be avoided.
 - Ctrl+H - Backspace,
@@ -72,6 +87,6 @@ The following Ctrl keybindings are considered risky and are advised to be avoide
 - Ctrl+Enter - Enter,
 - Ctrl+[1|2|3|4|5|6|7|8|9|0] - runes and control sequences.
 
-Please note that all window manager or custom terminal emulator keybindings also won't reach enix, as your window manager or
-terminal emulator consumes them.`,
+Please note that all window manager or custom terminal emulator keybindings also won't reach enix.
+The window manager or terminal emulator consumes them.`,
 }
