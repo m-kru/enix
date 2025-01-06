c8ee8ff964fae91980d92532a3654e11e8a613b6
Michał Kruszewski <mkru@protonmail.com>
2025-01-06 19:30:53 +0100

    help" Improve help messages

--- internal/help/cmd.go
+++ internal/help/cmd.go
@@ -8,24 +8,22 @@ var Commands = map[string]string{
 
 	// Command
 
-	"cmd": `cmd # Starts command prompt.`,
+	"cmd": `cmd # Starts command prompt.
+The command does not work in the prompt, as the prompt is already active.`,
 
 	"cmd-error": `cmd-error # Displays error message in the command prompt.
-This command is useful for enix debugging and development.`,
+The command is useful for enix debugging and development.`,
 
 	"cmd-info": `cmd-info # Displays message in the command prompt.
-This command is useful for enix debugging and development.`,
-
-	"cmd-list": `cmd-list # Lists all available commands in alphabetical order with one sentence summary.`,
+The command is useful for enix debugging and development.`,
 
 	"cmd-prev": `cmd-prev # Executes previous command.`,
 
 	// Cursor
 
-	"add-cursor": `add-cursor L [C=1] # Creates new cursor at line L and column C.
-If L is greater than the number of lines in the file, then the cursor is placed
-in the last line. If C is greater than the number of columns in a given line,
-then the cursor is placed in the last column.`,
+	"add-cursor": `add-cursor L [C=1] # Creates a new cursor at line L and column C.
+If L exceeds than the number of lines in the tab, the cursor is placed in the last line.
+If C esceeds than the number of columns in a given line, the cursor is placed in the last column.`,
 
 	"cursor-count": `cursor-count # Prints the number of cursors in the current tab.`,
 
@@ -33,9 +31,9 @@ then the cursor is placed in the last column.`,
 If cursor is in the last line, nothing happens.`,
 
 	"dump-cursor": `dump-cursor N # Dumps Nth cursor data.
-This command is useful for enix debugging and development.`,
+The command is useful for enix debugging and development.`,
 
-	"g": `g # An alias to the go command.`,
+	"g": `g # An alias for the 'go' command.`,
 
 	"go": `go position|mark-name # Goes to the position or restores a mark.
 Valid syntaxes for the position are:
@@ -45,12 +43,12 @@ Valid syntaxes for the position are:
   - go -1    # Goes to the last line column 1,
   - go -1 -1 # Goes to the last line last column,
   - go -1:-1 # Goes to the last line last column,
-  - go tmp   # Goes to mark named tmp.`,
+  - go tmp   # Goes to the mark named "tmp".`,
 
 	"left": `left # Moves cursor left.
-If cursor is in the first column of a line, then it is moved into the last
-column of the previous line. Unless this is the first line. In such a case,
-nothing happens.`,
+If the cursor is in the first column of a line, it is moved to the last column of the previous line.
+Unless this is the first line.
+In such a case, nothing happens.`,
 
 	"line-down": `line-down # Moves line down by one line.`,
 
@@ -66,9 +64,9 @@ then line-start moves cursor to the first line rune, even if it is a whitespace.
 	"prev-word-start": `word-start # Moves cursor to the previous word start.`,
 
 	"right": `right # Moves cursor right.
-If cursor is in the last column of a line, then it is moved into the first
-column of the next line. Unless this is the lastt line. In such a case,
-nothing happens.`,
+If cursor is in the last column of a line, it is moved into the first column of the next line.
+Unless this is the lastt line.
+In such a case, nothing happens.`,
 
 	"spawn-down": `spawn-down # Spawns a new cursor in the below line.`,
 
@@ -85,51 +83,50 @@ If cursor is in the fisrt line, nothing happens.`,
 
 	"backspace": `del # Deletes text before cursor or selected text.`,
 
-	"del": `del # Deletes text under cursor/selection.`,
+	"del": `del # Deletes text under cursor or selection.`,
 
-	"del-line": `del-line # Deletes lines with cursor/selection.`,
+	"del-line": `del-line # Deletes lines with cursor or selection.`,
 
-	"del-word": `del-word # Deletes words under cursor/selection.`,
+	"del-word": `del-word # Deletes words under cursor or selection.`,
 
 	"trim": `trim # Trims trailing whitespaces from all lines.`,
 
 	// File
 
-	"edit": `edit path/to/file ... # Opens a file in a new tab.
-If path to file is not provided, then it opens a new empty tab.
+	"e": `e # An alias for the 'edit' command.`,
 
-The command is named 'edit' instead of 'open' for the following reason.
+	"edit": `edit path/to/file ... # Opens a file in a new tab.
+If path to the file is not provided, it opens a new empty tab.
+
+The command is named "edit" instead of "open" for the following reason.
 If you have an alias for the enix in a shell, then the alias is probably 'e'.
 The edit command has an alias 'e' inside the enix.
-This means that opening a file in a shell and the enix has the same syntax when using aliases.
-`,
+This means that opening a file in a shell and the enix has the same syntax when using aliases.`,
 
-	"e": `o # An alias to the open command.`,
-
-	"save": `save [path/to/file] # Saves file using the provided path to file.
-If path to file is not provided, then it uses the current file path.`,
+	"save": `save [path/to/file] # Saves tab to the file using the provided path.
+If the path to a file is not provided, then it uses the current file path.`,
 
 	"type": `type type # Enforces file type.`,
 
 	// Find
 
-	"find-next": "find-next # Selects next find.",
+	"find-next": "find-next # Selects the next find.",
 
-	"find-sel-next": "find-sel-next # Preserves selections and selects next find.",
+	"find-sel-next": "find-sel-next # Preserves selections and selects the next find.",
 
-	"find-prev": "find-prev # Selects previous find.",
+	"find-prev": "find-prev # Selects the previous find.",
 
-	"find-sel-prev": "find-sel-prev # Preserves selections and selects previous find.",
+	"find-sel-prev": "find-sel-prev # Preserves selections and selects the previous find.",
 
 	// Indent
 
-	"deindent": `deindent # Decreases indent of lines with cursor.
-In the case of selections, it deindents all selected lines. Even if the
-selection starts/ends in the middle of a line.`,
+	"deindent": `deindent # Decreases the indent of lines with cursor.
+In the case of selections, it deindents all selected lines.
+Even if the selection starts or ends in the middle of a line.`,
 
 	"indent": `indent # Increases indent of lines with cursor.
-In the case of selections, it increases all selected lines. Even if the
-selection starts/ends in the middle of a line.`,
+In the case of selections, it increases all selected lines.
+Even if the selection starts or ends in the middle of a line.`,
 
 	// Insert
 
@@ -157,7 +154,7 @@ selection starts/ends in the middle of a line.`,
 
 	// Miscellaneous
 
-	"a": `a # An alias to the align command.`,
+	"a": `a # An alias for the 'align' command.`,
 
 	"align": `align # Aligns columns of cursors.`,
 
@@ -171,16 +168,16 @@ For example, by executing commands 'sel-right' and 'sel-left.`,
 	"dump-colors": `dump-colors # Dumps colorscheme configuration to JSON format.`,
 
 	"esc": `esc # Escapes the current context.
-The actual action depends on the context. For example, if the focues is on the
-command prompt, then escape command escapes the command prompt and allows user
-to continue file editing. If the focus in on a file editing and there is any
-selection, then escape command clears all selections.`,
+The actual action depends on the context.
+For example, if the focus is on the command prompt, then the command escapes the command prompt and allows user to continue tab editing.
+If the focus in on a tab editing and there is any selection, then the command clears all selections.`,
 
-	"h": `h # An alias to the help command.`,
+	"h": `h # An alias for the 'help' command.`,
 
 	"help": `help topic|command-name # Displays help message for a given topic or command in a new tab.
-If neither topic nor command name is provided displays help message
-for the help command. The same as 'help help'. Valid topics are:
+If neither topic nor command name is provided, the command displays help message for the 'help' command.
+The same as 'help help' command would do.
+Valid topics are:
   - commands - list of available commands with their synopsys,
   - config - general description of enix configuration,
   - cursors - what they are and how they work,
@@ -188,40 +185,40 @@ for the help command. The same as 'help help'. Valid topics are:
   - keybindings - how to set, and what to watch out for,
   - selections - what they are and how they work.`,
 
-	"join": `join # Joins line with cursor with below line.`,
+	"join": `join # Joins line with cursor with the below line.`,
 
 	"key-name": `key-name # Returns name of the pressed key combo.
-The command opens a new tab named "key-name". On each keystroke a new line
-is added to the tab with the key combo name. Once user leaves the tab key-name
-state by hitting the escape key, it can only be reentered by executing the
-key-name command again.`,
+The command opens a new tab named "key-name".
+On each keystroke a new line is added to the tab with the key combo name.
+Once user leaves the tab key-name state by hitting the escape key, it can only be reentered by executing the command again.`,
 
-	"mark": `mark name # Creates new named mark.
-Marks allow to record current cursors or selections positions.
-Mark name must not start with a digit or '-' rune. To restore marks one
-has to use the go command providing as an argument the name of a mark.`,
-	"m": `m # An alias to the mark command.`,
+	"m": `m # An alias for the 'mark' command.`,
+
+	"mark": `mark name # Creates a new named mark.
+Marks allow to record current cursor or selection position.
+Mark name must not start with a digit or '-' rune.
+To restore marks one has to use the 'go' command providing as an argument the name of a mark.`,
 
 	"pwd": `pwd # Prints working directory.`,
 
-	"q": `q # An alias to the quit command.`,
+	"q": `q # An alias for the 'quit' command.`,
 
-	"quit": `quit # Quits tab.
-If the tab is the last tab opened tab, then quit also quits the enix editor.
-Quit returns an error if current tab has unsaved changes.`,
+	"quit": `quit # Quits the current tab.
+If the tab is the last tab, then the command also quits the enix editor.
+The command returns an error if current tab has unsaved changes.`,
 
-	"q!": `q! # An alias to the quit! command.`,
+	"q!": `q! # An alias for the 'quit!' command.`,
 
-	"quit!": `quit! # Force quit.,
-Forced version of the quit command. It quits the tab even if there are unsaved changes.`,
+	"quit!": `quit! # Forces quit.,
+Forced version of the 'quit' command.
+The command quits the tab even if there are unsaved changes.`,
 
 	"replace": `replace # Replaces rune under cursor or runes under selection.
-The command first deletes rune under the cursor, or runes under selection, and then
-enters the insert moode for a single insertion.`,
+The command first deletes runes under the cursor or selection, and then enters the insert moode for a single insertion.`,
 
 	"sh": `sh [-i] cmd [arg] ... # Executes command cmd in the shell.
-The sh command obtains a shell via the $SHELL environment variable.
-If this variable is not set, then the command returns an error.
+The command obtains a shell via the $SHELL environment variable.
+If this variable is not set, the command returns an error.
 
 The text directed by the command to the stdout gets pasted into the tab.
 If the text ends with the newline character, then the line-based paste is used.
@@ -244,26 +241,26 @@ The -i flag controls whether current indent shall be added to the text generated
 For cursors, current indent is the indent of a line with the cursors.
 For selections, current indent is the indent of the first line of the selection.
 
-The sh command can be used for various purposes and is the only official plug-in system for enix.
-For example, the sh command can be used with https://github.com/m-kru/tmpl for template inserting.
+The command can be used for various purposes and is the only official plug-in system for enix.
+For example, the command can be used with https://github.com/m-kru/tmpl for template inserting.
 
 Before executing the command, enix sets the following environment variables.
   - ENIX_FILEPATH - absolute path to the current tab file,
   - ENIX_FILETYPE - file type of the current tab.
-These variables can be used not only within the executed program, but also within the enix sh command.
+These variables can be used not only within the executed program, but also within the enix 'sh' command.
 As these are environment variables, they will be expanded by the shell.`,
 
-	"suspend": `suspend # Stops process and gives control to shell.
-This command has been so far tested only on Linux.`,
+	"suspend": `suspend # Stops enix and gives control to the shell.
+The command has been so far tested only on Linux.`,
 
 	"trim-on-save": `trim-on-save [value] # Sets the TrimOnSave config.
 Possible values are 0 for false, and ony other integer value for true.
-If value is not present, then the command returns current value.`,
+If value is not present, the command returns current value.`,
 
 	"undo": `undo # Undos last action modifying the tab content.`,
 
-	"yank": `yank # Copies selected text.
-For cursors, the yank command copies all lines containing cursors.
+	"yank": `yank # Copies line with cursor or selection.
+For cursors, the command copies all lines containing cursors.
 This is because the user rarely wants to copy a single rune.
 On the other hand, copying a single line is a very common action.
 Copying a single rune is still possible by creating a selection of width 1.
@@ -272,67 +269,68 @@ For example, by executing commands 'sel-right' and 'sel-left.`,
 	// Selection
 
 	"sel": `sel regex # Selects all finds of regex pattern within selections.
-If there are no selections within the tab, then the whole tab is searched.`,
+If there are no selections within the tab, the whole tab is searched.`,
 
 	"sel-all": `sel-all # Selets all tab content.`,
 
 	"sel-count": `sel-count # Prints the number of selections in the current tab.`,
 
-	"sel-left": `sel-left # Extends/shrinks selection one rune left.`,
+	"sel-left": `sel-left # Extends or shrinks selection one rune left.`,
 
 	"sel-line": `sel-line # Selects whole line.
-If whole current line is already selected, then it extends the selection with the next line.`,
+If whole current line is already selected, it extends the selection with the next line.`,
 
-	"sel-right": `sel-right # Extends/shrinks selection one rune right.`,
+	"sel-right": `sel-right # Extends or shrinks selection one rune right.`,
 
 	"sel-white": `sel-white # Selects sequence of whitespaces.
 If cursors is not placed on a whitespace character, nothing is selected.`,
 
-	"sel-prev-word-start": `sel-prev-word-start # Selects or extends selection to the start of previous  word.`,
+	"sel-prev-word-start": `sel-prev-word-start # Selects, extends or shrinks selection to the start of the previous word.`,
 
 	"sel-to-tab": `sel-to-tab [tab-path] # Opens a new tab with the content of current selections.`,
 
 	"sel-word": `sel-word # Selects word under cursor.`,
 
-	"sel-word-end": `sel-word-end # Selects or extends selection to the end of next word.`,
+	"sel-word-end": `sel-word-end # Selects, extends or shrinks selection to the end of next word.`,
 
 	"sel-switch-cursor": `sel-switch-cursor # Changes position of the cursor within selection.`,
 
 	// Tab
 
-	"tab-count": `tab-count # Prints the number of opened tabs.`,
+	"tab-count": `tab-count # Prints the number of tabs.`,
+
+	"tn": `tn # An alias for the 'tab-next' command.`,
 
 	"tab-next": `tab-next # Cycles to the next tab.
 If the current tab is the last tab, then it wraps to the first tab.`,
 
-	"tn": `tn # An alias to the tab-next command.`,
-
 	"tab-move": `tab-move N [pattern] # Moves tab which name matches pattern to position N.
-If pattern is not provided, then current tab is moved. The pattern must be
-unambiguous. If N is not provided, then it is assumed to be equal 1.`,
+If pattern is not provided, then current tab is moved.
+The pattern must be unambiguous.
+If N is not provided, it is assumed to equal 1.`,
 
 	"tab-open": `tab-open # Opens a new empty tab.`,
 
-	"tab-prev": `tab-prev # Cycles to the previous tab.
-If the current tab is the first tab, then it wraps to the last tab.`,
+	"tp": `tp # An alias for the 'tab-prev' command.`,
 
-	"tp": `tp # An alias to the tab-prev command.`,
+	"tab-prev": `tab-prev # Cycles to the previous tab.
+If the current tab is the first tab, it wraps to the last tab.`,
 
 	"tab-switch": `tab-switch pattern1 [pattern2] # Switch tabs which names match patterns.
-The patterns must be unambiguous. If the second is absent, then it is assumed
-to be the current tab.`,
+The patterns must be unambiguous.
+If the second is absent, it is assumed to be the current tab.`,
 
 	// View
 
-	"view": "view line-number column # Sets view start at provided line number and column.",
+	"view": "view line-number column # Sets the view start at provided line number and column.",
 
-	"view-center": "view-center # Centers view.",
+	"view-center": "view-center # Centers the view.",
 
-	"view-down": "view-down # Scrolls view down by one line.",
+	"view-down": "view-down # Scrolls the view down by one line.",
 
-	"view-left": "view-left # Scrolls view left by one column.",
+	"view-left": "view-left # Scrolls the view left by one column.",
 
-	"view-right": "view-right # Scrolls view right by one column.",
+	"view-right": "view-right # Scrolls the view right by one column.",
 
-	"view-up": "view-up # Scrolls view up by one line.",
+	"view-up": "view-up # Scrolls the view up by one line.",
 }
