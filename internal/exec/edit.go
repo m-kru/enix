bbcf9c96552e31d15c705b9ec5c4316aa5e4b75b
Michał Kruszewski <mkru@protonmail.com>
2025-01-07 18:19:20 +0100

    close #56: Fix freeze when opening the same file twice

--- internal/exec/edit.go
+++ internal/exec/edit.go
@@ -2,6 +2,9 @@ package exec
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/m-kru/enix/internal/tab"
 )
 
@@ -14,6 +17,38 @@ func Edit(args []string, t *tab.Tab) (*tab.Tab, error) {
 
 	errMsg := ""
 	for i, path := range args {
+		// Check if tab with given path already exists
+		abspath := path
+		if !filepath.IsAbs(abspath) {
+			wd, err := os.Getwd()
+			if err != nil {
+				return nil, err
+			}
+			abspath = filepath.Join(wd, path)
+		}
+		t2 := t.First()
+		for {
+			if t2 == nil {
+				break
+			}
+
+			abspath2 := t2.Path
+			if !filepath.IsAbs(abspath2) {
+				wd, err := os.Getwd()
+				if err != nil {
+					return nil, err
+				}
+				abspath2 = filepath.Join(wd, abspath2)
+			}
+
+			if abspath == abspath2 {
+				return t2, nil
+			}
+
+			t2 = t2.Next
+		}
+
+		// Open new tab
 		newT, err := tab.Open(t.Frame, path)
 		if newT != nil {
 			t.Append(newT)
