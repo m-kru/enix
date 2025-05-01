d54f856de5466cc242b185dd0e525eb3301bd222
Michał Kruszewski <mkru@protonmail.com>
2025-05-01 12:40:51 +0200

    lang: Remvoe analyze.go file

--- internal/lang/highlighter.go
+++ internal/lang/highlighter.go
@@ -69,6 +69,31 @@ func NewHighlighter(lang string) (*Highlighter, error) {
 	return hl, nil
 }
 
+// The line argument  must be the first line of file.
+// StartLineIdx is the index of the first visible line.
+// EndLineIdx is the index of the last visible line.
+func (hl *Highlighter) Analyze(
+	line *line.Line, // First tab line
+	firstVisLineNum int,
+	lastVisLineNum int,
+	cursors []*cursor.Cursor,
+) []highlight.Highlight {
+	hls := make([]highlight.Highlight, 0, 1024)
+
+	if len(hl.Regions) == 0 {
+		return nil
+	}
+
+	hl.reset(cursors, firstVisLineNum, lastVisLineNum)
+
+	for !hl.done() {
+		hl.analyzeLine(line, &hls)
+		line = line.Next
+	}
+
+	return hls
+}
+
 // Resets highlighter for a new analysis.
 func (hl *Highlighter) reset(
 	cursors []*cursor.Cursor,
